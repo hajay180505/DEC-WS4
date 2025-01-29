eb4ed25d5cd5756cfc52023257f3126249bc56b6
Ajay H <hajay180505@gmail.com>
2025-01-29 22:14:36 +0530

    Working UI

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# DEC-WS4
-Spotify clone-ish
+# Spotify Clone with ReactJS
+In this Spotify Clone application you can log in to the application with your spotify account and see your own playlist.
+
+## Spotify Clone Demo Link
+<a href="https://spotify-clone-b6901.web.app/">click me</a> for demo.
+
+
+## Topics
++ Spotify Authentication
++ Spotify API
++ Context API (Redux Pattern)
++ Material UI
++ 3rd party API
++ Responsive Design
+
+## Author
+Mert Çankaya
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { DataLayer } from './DataLayer';
+import reducer, {initialState} from "./reducer"
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
+    <DataLayer initialState={initialState} reducer={reducer}>
     <App />
-  </React.StrictMode>
+    </DataLayer>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
